ce52ef0e23d1a35682493a27596c027b881900a0
Jon-Mark Hampson <33136955+Jon-MarkHampson@users.noreply.github.com>
2022-03-25 22:58:03 +0000

    initial commit

--- main.cpp
+++ main.cpp
@@ -56,46 +56,66 @@ Purpose:  This project will show you the difference between member functions and
 #include <string>
 struct T
 {
-    T(<#type name#> v, const char* <#variable name#>)   //1
-    //2
-    //3
+    int value;//2
+    std::string name;//3
+    T(int v, const char* namePtr): value(v), name(namePtr){}  //1
 };
 
-struct <#structName1#>                                //4
+struct Comparer                                //4
 {
-    <#type name#> compare(<#type name#> a, <#type name#> b) //5
+    T* compare(T* a, T* b) //5
     {
+        if( ( a != nullptr ) && ( b != nullptr) )
+        {
         if( a->value < b->value ) return a;
         if( a->value > b->value ) return b;
+        }
         return nullptr;
     }
 };
 
 struct U
 {
-    float <#name1#> { 0 }, <#name2#> { 0 };
-    <#returnType#> <#memberFunction#>(<#type name#>* <#updatedValue#>)      //12
+    float a { 0 }, b { 0 };
+    float converge(float* updatedVal)      //12
     {
-        
+        if(updatedVal != nullptr)
+        {
+            std::cout << "U's a value: " << this->a << std::endl;
+            this->a = *updatedVal;
+            std::cout << "U's a updated value: " << this->a << std::endl;
+            while( std::abs(this->b - this->a) > 0.001f )
+            {
+                /*
+                 write something that makes the distance between that->b and that->a get smaller
+                 */
+                this->b += 0.001f;
+            }
+        }
+        std::cout << "U's b updated value: " << this->b << std::endl;
+        return this->b * this->a;
     }
 };
 
-struct <#structname2#>
+struct Converger
 {
-    static <#returntype#> <#staticFunctionA#>(U* that, <#type name#>* <#updatedValue#> )        //10
+    static float converge(U* that, float* updatedVal )        //10
     {
-        std::cout << "U's <#name1#> value: " << that-><#name1#> << std::endl;
-        that-><#name1#> = <#updatedValue#>;
-        std::cout << "U's <#name1#> updated value: " << that-><#name1#> << std::endl;
-        while( std::abs(that-><#name2#> - that-><#name1#>) > 0.001f )
+        if(that != nullptr && updatedVal != nullptr)
         {
-            /*
-             write something that makes the distance between that-><#name2#> and that-><#name1#> get smaller
-             */
-            that-><#name2#> += ;
+            std::cout << "U's a value: " << that->a << std::endl;
+            that->a = *updatedVal;
+            std::cout << "U's a updated value: " << that->a << std::endl;
+            while( std::abs(that->b - that->a) > 0.001f )
+            {
+                /*
+                write something that makes the distance between that->b and that->a get smaller
+                */
+                that->b += 0.1f;
+            }
         }
-        std::cout << "U's <#name2#> updated value: " << that-><#name2#> << std::endl;
-        return that-><#name2#> * that-><#name1#>;
+        std::cout << "U's b updated value: " << that->b << std::endl;
+        return that->b * that->a;
     }
 };
         
@@ -104,7 +124,7 @@ struct <#structname2#>
 
  Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
  
- If you didn't already: 
+ If you didn't already:
     Make a pull request after you make your first commit
     pin the pull request link and this repl.it link to our DM thread in a single message.
 
@@ -115,17 +135,20 @@ struct <#structname2#>
 
 int main()
 {
-    T <#name1#>( , );                                             //6
-    T <#name2#>( , );                                             //6
+    T t1(73, "Colin");                                             //6
+    T t2(71, "Lynn");                                             //6
     
-    <#structName1#> f;                                            //7
-    auto* smaller = f.compare( , );                              //8
-    std::cout << "the smaller one is << " << smaller->name << std::endl; //9
+    Comparer f;                                            //7
+    auto* smaller = f.compare(&t1, &t2);                              //8
+    if(smaller != nullptr)
+        std::cout << "the smaller one is << " << smaller->name << std::endl; //9
+    else
+        std::cout << "Warning: smaller is pointing to a nullptr which was returned from f.compare()." << std::endl;
     
-    U <#name3#>;
+    U u1;
     float updatedValue = 5.f;
-    std::cout << "[static func] <#name3#>'s multiplied values: " << <#structname2#>::<#staticFunctionA#>( , ) << std::endl;                  //11
+    std::cout << "[static func] u1's multiplied values: " << Converger::converge(&u1, &updatedValue) << std::endl;                  //11
     
-    U <#name4#>;
-    std::cout << "[member func] <#name4#>'s multiplied values: " << <#name4#>.<#memberFunction#>( &updatedValue ) << std::endl;
+    U u2;
+    std::cout << "[member func] u2's multiplied values: " << u2.converge( &updatedValue ) << std::endl;
 }
